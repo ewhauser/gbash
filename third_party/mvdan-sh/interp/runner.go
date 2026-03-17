bc9b9c523878e7086cf7d4ad65d315ef82f7851d
Eric Hauser <ewhauser@gmail.com>
2026-03-17 16:13:56 -0600

    interp: preserve prefix assign side effects for decl builtins

--- third_party/mvdan-sh/interp/runner.go
+++ third_party/mvdan-sh/interp/runner.go
@@ -499,6 +499,15 @@ func (r *Runner) cmd(ctx context.Context, cm syntax.Command) {
 		}
 		r.lastExpandExit = exitStatus{}
 		fields := r.fields(args...)
+		if decl := callExprDeclClause(args); decl != nil {
+			r.runCallAssigns(cm.Assigns)
+			if r.exit.fatalExit || r.exit.exiting {
+				return
+			}
+			r.exit.clear()
+			r.cmd(ctx, decl)
+			return
+		}
 		if len(fields) == 0 {
 			for _, as := range cm.Assigns {
 				prev := r.lookupVar(as.Name.Value)
@@ -550,24 +559,7 @@ func (r *Runner) cmd(ctx context.Context, cm syntax.Command) {
 			break
 		}
 
-		type restoreVar struct {
-			name string
-			vr   expand.Variable
-		}
-		var restores []restoreVar
-
-		for _, as := range cm.Assigns {
-			name := as.Name.Value
-			prev := r.lookupVar(name)
-
-			vr := r.assignVal(prev, as, "")
-			// Inline command vars are always exported.
-			vr.Exported = true
-
-			restores = append(restores, restoreVar{name, prev})
-
-			r.setVar(name, vr)
-		}
+		restores := r.runCallAssigns(cm.Assigns)
 
 		trace.call(fields[0], fields[1:]...)
 		trace.newLineFlush()
@@ -1088,6 +1080,53 @@ func (r *Runner) flattenAssigns(args []*syntax.Assign) iter.Seq[*syntax.Assign] 
 			}
 		}
 	}
+}
+
+type restoreVar struct {
+	name string
+	vr   expand.Variable
+}
+
+func (r *Runner) runCallAssigns(assigns []*syntax.Assign) []restoreVar {
+	var restores []restoreVar
+	for _, as := range assigns {
+		name := as.Name.Value
+		prev := r.lookupVar(name)
+
+		vr := r.assignVal(prev, as, "")
+		// Inline command vars are always exported.
+		vr.Exported = true
+
+		restores = append(restores, restoreVar{name, prev})
+		r.setVar(name, vr)
+	}
+	return restores
+}
+
+func callExprDeclClause(args []*syntax.Word) *syntax.DeclClause {
+	if len(args) == 0 {
+		return nil
+	}
+	name := args[0].Lit()
+	switch name {
+	case "declare", "local", "export", "readonly", "typeset", "nameref":
+	default:
+		return nil
+	}
+	decl := &syntax.DeclClause{
+		Variant: &syntax.Lit{
+			ValuePos: args[0].Pos(),
+			ValueEnd: args[0].End(),
+			Value:    name,
+		},
+	}
+	for _, arg := range args[1:] {
+		decl.Args = append(decl.Args, &syntax.Assign{
+			Naked: true,
+			Value: arg,
+		})
+	}
+	return decl
 }
 
 // reparseCompoundAssign checks if an assign's value looks like a compound
