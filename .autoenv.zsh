cd1417955db838a858c355e536d44f3d0c3e687e
Eric Hauser <ewhauser@gmail.com>
2026-03-17 13:48:18 -0600

    fix nix autoenv

--- .autoenv.zsh
+++ .autoenv.zsh
@@ -2,5 +2,5 @@ if command -v nix >/dev/null 2>&1; then
   _AUTOENV_SNAPSHOT=$(mktemp "${TMPDIR:-/tmp}/autoenv.XXXXXX")
   export _AUTOENV_SNAPSHOT
   typeset -px > "$_AUTOENV_SNAPSHOT"
-  eval "$(nix print-dev-env)"
+  eval "$(nix print-dev-env | grep -v 'LINENO=')"
 fi
