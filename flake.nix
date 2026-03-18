ee5f731b663faf0a8c22ab90ffb1bc9114671c4e
Eric Hauser <ewhauser@gmail.com>
2026-03-18 11:22:08 -0600

    nix: add coreutils-test-suite package and R2 binary cache (#451)
    Add coreutils-test-suite package to flake that builds GNU coreutils 9.10
    with the full test suite (matching the Dockerfile build). Configure
    Cloudflare R2 as a binary cache substituter.
    
    - Add coreutils-test-suite package with test suite preserved
    - Add nixConfig for R2 cache substituter
    - Add make targets: nix-build, nix-cache
    - Add GitHub Actions workflow for Linux cache builds

--- flake.nix
+++ flake.nix
@@ -3,6 +3,15 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+  nixConfig = {
+    extra-substituters = [
+      "s3://gbash-nix-cache?profile=r2&endpoint=aea0a2c4e5c5c74a3e84a12c855a7e37.r2.cloudflarestorage.com"
+    ];
+    extra-trusted-public-keys = [
+      "gbash-cache-1:snMAp1ltEdxgvSRm9R5SCPk3Dpi/Za9WGPT8NJaEx7c="
+    ];
+  };
+
   outputs = { self, nixpkgs }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
@@ -11,6 +20,49 @@
       packages = forAllSystems (pkgs: {
         bash = pkgs.bash;
         bats = pkgs.bats;
+
+        # Full coreutils build tree including test suite (matches Dockerfile)
+        coreutils-test-suite = pkgs.stdenv.mkDerivation rec {
+          pname = "coreutils-test-suite";
+          version = "9.10";
+
+          src = pkgs.fetchurl {
+            url = "https://ftp.gnu.org/gnu/coreutils/coreutils-${version}.tar.gz";
+            sha256 = "e0bde1fb68509447fc723cf2517e8a8c7fa46769919bb7490ed350a2e9238562";
+          };
+
+          nativeBuildInputs = with pkgs; [
+            perl
+            gawk
+          ];
+
+          buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
+            acl
+            libselinux
+          ]);
+
+          configureFlags = [
+            "--disable-nls"
+            "--disable-dependency-tracking"
+          ];
+
+          # Disable format-security warning treated as error (clang is stricter than GCC)
+          env.NIX_CFLAGS_COMPILE = "-Wno-format-security";
+
+          # Build but don't install - keep the full source tree with build artifacts
+          buildPhase = ''
+            make -j$NIX_BUILD_CORES
+          '';
+
+          # Copy entire build tree to output
+          installPhase = ''
+            mkdir -p $out
+            cp -r . $out/
+          '';
+
+          # Skip fixup to preserve the build tree structure
+          dontFixup = true;
+        };
       });
 
       devShells = forAllSystems (pkgs: {
