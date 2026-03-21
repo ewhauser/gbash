b6bf976993caa4149a1752dfb2fdde402b03cded
Eric Hauser <ewhauser@gmail.com>
2026-03-21 08:00:21 -0600

    conformance: normalize job-control process groups

--- internal/conformance/runner.go
+++ internal/conformance/runner.go
@@ -31,6 +31,7 @@ var bashShellPrefixPattern = regexp.MustCompile(`^(?:[^:\n]+/)?[A-Za-z0-9_-]*sh:
 var bashAnsiCQuotedCommandNotFoundPattern = regexp.MustCompile(`\$'((?:[^'\\]|\\.)*)': command not found`)
 var bashQuotedNoSuchFilePattern = regexp.MustCompile(`(?m)^([-.[:alnum:]_]+): '([^']+)': No such file or directory$`)
 var bashCannotOpenNoSuchFilePattern = regexp.MustCompile(`(?m)^([-.[:alnum:]_]+): cannot (?:open|remove) '([^']+)'(?: for reading)?: No such file or directory$`)
+var bashTerminalProcessGroupPattern = regexp.MustCompile(`cannot set terminal process group \(\d+\)`)
 var procFDPathPattern = regexp.MustCompile(`/proc/\d+/fd`)
 var procFDLsMissingPattern = regexp.MustCompile(`(?m)^ls: /proc/PID/fd(?:[^\n]*)?: No such file or directory\n?`)
 var (
@@ -540,6 +541,7 @@ func normalizeBashStderr(value string) string {
 	value = normalizeMultiLineNestedShellPrefixes(value)
 	value = normalizeCommandStringOperandPrefix(value)
 	value = normalizeArithOperandContinuation(value)
+	value = bashTerminalProcessGroupPattern.ReplaceAllString(value, "cannot set terminal process group (PGID)")
 	value = procFDLsMissingPattern.ReplaceAllString(value, "")
 	value = strings.ReplaceAll(value, "shopt: usage: shopt [-pqsu] [-o long-option] optname [optname...]\n", "shopt: usage: shopt [-pqsu] [-o] [optname ...]\n")
 	value = bashCannotOpenNoSuchFilePattern.ReplaceAllString(value, "$1: $2: No such file or directory")
